0082d670494c167aec82d1988f2446b77c89a243
Mats Höhn <mats.hohn@student.hogent.be>
2023-02-27 02:25:50 +0100

    c

---
+++ Connectie.java
@@ -0,0 +1,9 @@
+package persistentie;
+
+public class Connectie {
+
+    /*
+     * INFO: Hieronder dien je uiteraard het schema, de username en het paswoord nog aan te passen.
+     */
+    public static final String JDBC_URL = "jdbc:mysql://ID340733_g26.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID340733_g26&password=rummikub26";
+}

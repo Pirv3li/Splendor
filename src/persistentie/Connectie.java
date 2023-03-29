57bd9ab276023279e627291db053990db946050c
Papuna Noniashvili <papuna.noniashvili@student.hogent.be>
2023-03-29 19:27:50 +0200

    Connectie aangepast naar onze databank

--- src/persistentie/Connectie.java
+++ src/persistentie/Connectie.java
@@ -1,9 +1,5 @@
 package persistentie;
 
 public class Connectie {
-
-    /*
-     * INFO: Hieronder dien je uiteraard het schema, de username en het paswoord nog aan te passen.
-     */
-    public static final String JDBC_URL = "jdbc:mysql://ID340733_g26.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID340733_g26&password=rummikub26";
+    public static final String JDBC_URL = "jdbc:mysql://ID399806_g61.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID399806_g61&password=5iG4T8N7IZoL0rt16dRQ";
 }

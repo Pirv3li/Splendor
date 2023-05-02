23576d34422d51b5d0674936e1f39989671856d3
MATTIS HANSEN <mattis.hansen@student.hogent.be>
2023-05-02 10:25:38 +0200

    Spelbord

---
+++ src/gui/SpelBordController.java
@@ -0,0 +1,245 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class SpelBordController {
+
+    @FXML
+    private VBox VBoxSpeler1;
+
+    @FXML
+    private VBox VBoxSpeler2;
+
+    @FXML
+    private VBox VBoxSpeler3;
+
+    @FXML
+    private VBox VBoxSpeler4;
+
+    @FXML
+    private ImageView imgvNiveau1;
+
+    @FXML
+    private ImageView imgvNiveau2;
+
+    @FXML
+    private ImageView imgvNiveau3;
+
+    @FXML
+    private ImageView imgvSpeler1Kaart1;
+
+    @FXML
+    private ImageView imgvSpeler1Kaart2;
+
+    @FXML
+    private ImageView imgvSpeler1Kaart3;
+
+    @FXML
+    private ImageView imgvSpeler1Kaart4;
+
+    @FXML
+    private ImageView imgvSpeler1Kaart5;
+
+    @FXML
+    private ImageView imgvSpeler2Kaart1;
+
+    @FXML
+    private ImageView imgvSpeler2Kaart2;
+
+    @FXML
+    private ImageView imgvSpeler2Kaart3;
+
+    @FXML
+    private ImageView imgvSpeler2Kaart4;
+
+    @FXML
+    private ImageView imgvSpeler2Kaart5;
+
+    @FXML
+    private ImageView imgvSpeler3Kaart1;
+
+    @FXML
+    private ImageView imgvSpeler3Kaart2;
+
+    @FXML
+    private ImageView imgvSpeler3Kaart3;
+
+    @FXML
+    private ImageView imgvSpeler3Kaart4;
+
+    @FXML
+    private ImageView imgvSpeler3Kaart5;
+
+    @FXML
+    private ImageView imgvSpeler4Kaart1;
+
+    @FXML
+    private ImageView imgvSpeler4Kaart2;
+
+    @FXML
+    private ImageView imgvSpeler4Kaart3;
+
+    @FXML
+    private ImageView imgvSpeler4Kaart4;
+
+    @FXML
+    private ImageView imgvSpeler4Kaart5;
+
+    @FXML
+    private Label lblAantalKaarten1;
+
+    @FXML
+    private Label lblAantalKaarten2;
+
+    @FXML
+    private Label lblAantalKaarten3;
+
+    @FXML
+    private Label lblDiamant;
+
+    @FXML
+    private Label lblOnyx;
+
+    @FXML
+    private Label lblOnyx1;
+
+    @FXML
+    private Label lblRobijn;
+
+    @FXML
+    private Label lblSaffier;
+
+    @FXML
+    private Label lblSmaragd;
+
+    @FXML
+    private Label lblSpeler1;
+
+    @FXML
+    private Label lblSpeler1AantalPunten;
+
+    @FXML
+    private Label lblSpeler1Kaarten;
+
+    @FXML
+    private Label lblSpeler1Kaarten1;
+
+    @FXML
+    private Label lblSpeler1Kaarten2;
+
+    @FXML
+    private Label lblSpeler1Kaarten3;
+
+    @FXML
+    private Label lblSpeler2;
+
+    @FXML
+    private Label lblSpeler2AantalPunten;
+
+    @FXML
+    private Label lblSpeler3;
+
+    @FXML
+    private Label lblSpeler3AantalPunten;
+
+    @FXML
+    private Label lblSpeler4;
+
+    @FXML
+    private Label lblSpeler4AantalPunten;
+
+    @FXML
+    void btnEdele1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEdele2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEdele3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGem1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGem2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGem3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGem4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGem5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGem6(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKaart1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKaart2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKaart3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKaart4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKaart5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKaart6(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKaart7(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKaart8(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKaart9(ActionEvent event) {
+
+    }
+
+}
